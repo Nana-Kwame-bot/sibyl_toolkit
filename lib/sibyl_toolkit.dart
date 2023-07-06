5913ffcb6fe3327a9f6d1bed92928002a9bce733
Nana Kwame <aduhenry9@gmail.com>
2023-07-06 00:39:29 +0000

    first commit

---
+++ lib/sibyl_toolkit.dart
@@ -0,0 +1,10 @@
+/// sibyl_toolkit, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate sibyl_toolkit
+/// dart pub global activate sibyl_toolkit
+///
+/// # see usage
+/// sibyl_toolkit --help
+/// ```
+library sibyl_toolkit;
